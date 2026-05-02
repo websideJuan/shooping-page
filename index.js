372d7d7bbbd68589c4601e5fb3e3aa787c93d7a4
juanHerrera <portador1051@gmail.com>
2026-05-01 21:18:33 -0400

    Optimizaciones globales

--- index.js
+++ index.js
@@ -47,10 +47,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function showCart() {
     const cartCollapse = document.getElementById("cart-collapse");
-
     if (cartCollapse.classList.contains("show")) {
+      cartCollapse.style.transitionDelay = '.6s'
+      cartCollapse.children[0].style.transitionDelay = '.0s'
       cartCollapse.classList.remove("show");
     } else {
+      console.log(cartCollapse.children[0].style);
+      
+      cartCollapse.style.transitionDelay = '.0s'
+      cartCollapse.children[0].style.transitionDelay = '.6s'
       cartCollapse.classList.add("show");
     }
   }
@@ -70,8 +75,19 @@ document.addEventListener("DOMContentLoaded", function () {
           <div class="card-body">
             <span style="color: lightgray">${cardProductItem.category}</span>
             <h4>${cardProductItem.title.length > 10 ? cardProductItem.title.slice(0, 10).concat("...") : cardProductItem.title}</h4>
-            
-            <p class="card-price" style="color: rosybrown; font-size: 20px;">$ ${cardProductItem.price.toLocaleString()}</p>
+            <p class="card-price" style="color: rosybrown; font-size: 20px;">$ ${(cardProductItem.price - (cardProductItem.price * cardProductItem.discount) / 100).toLocaleString()}</p>
+            ${
+              cardProductItem.discount <= 0
+                ? ""
+                : `
+                  <p style="position: absolute; top: 10px; left: 10px; background-color: red; color: white; padding: 3px 10px; font-weight: boldx">
+                    ${`${cardProductItem.discount}%`}
+                  </p>
+                  <p class="card-price" style="text-align: center; text-decoration: line-through;">
+                    ${cardProductItem.price.toLocaleString()}
+                  </p>
+                `
+            }
           </div>
           <div class="card-footer" >
             <button class="card-btn-add" data-id="${cardProductItem.id}">
@@ -158,8 +174,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const listCartProduct = document.createElement("ul");
     cartBody.innerHTML = "";
     cart.forEach((cartItem) => {
-      console.log(cartItem.price);
-
       listCartProduct.innerHTML += ` 
       <li class="cart-item" data-id=${cartItem.id}>
         <img
@@ -192,11 +206,10 @@ document.addEventListener("DOMContentLoaded", function () {
       </li>`;
     });
     cartBody.appendChild(listCartProduct);
-    totalProductsCart.innerHTML = cart.reduce(
-      (acc, current) => acc + current.price * current.count,
-      0,
-    ).toLocaleString()
-  
+    totalProductsCart.innerHTML = cart
+      .reduce((acc, current) => acc + current.price * current.count, 0)
+      .toLocaleString();
+
     actionCart(document.querySelectorAll(".cart-btn-control"));
     deleteCartItem(document.querySelectorAll(".cart-delete-item"));
     contatcForWstpp(cart);
